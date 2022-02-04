8050a634680fcf73f7febaa825c71d4b6d0a664b
chenmichael2 <chenmichael2012@gmail.com>
2022-02-03 17:52:06 -0800

    adding my picture

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 class Main extends Component {
     render() {
-        return(
+        return (
             <section className="main-start-section hero is-fullheight is-fullheight-with-navbar">
-                <h1 className="main-header">Hello Main Section</h1>
+                <div className="introduction-me">
+                    <h1 className="main-header">Michael Chen</h1>
+                    <img src='intro-michael.png' alt="Michael Chen"/>
+                </div>
             </section>
         )
     }
