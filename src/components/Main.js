253c9bfc6c2bc62a7386aa99ab3f3c7cdaa75903
chenmichael2 <chenmichael2012@gmail.com>
2022-02-05 14:00:12 -0800

    still working on the main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,15 @@ class Main extends Component {
         return (
             <section className="main-start-section hero is-fullheight is-fullheight-with-navbar">
                 {/* <div className="introduction-me"> */}
+
                 <img className="main-photo" src='IMG_6405.jpg' alt="Michael Chen" />
-                <div className="main-text-section">
-                    <h1 className="main-header">Michael Chen</h1>
-                    <p className="main-paragraph">Hi! I'm Michael Chen and I'm a recent software engineer graduate from General Assembly</p>
+                <div className="main-text-container">
+                    <div className="main-text-section">
+                        <h1 className="main-header">Michael Chen</h1>
+                        <p className="main-paragraph">Hi! I'm Michael Chen and I'm a recent software engineer graduate from General Assembly</p>
+                    </div>
                 </div>
+
                 {/* </div> */}
             </section>
         )
