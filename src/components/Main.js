9ac1b667dd9b7ce6e1c5c0c3d7f655b7d8defe2a
chenmichael2 <chenmichael2012@gmail.com>
2022-02-05 15:27:44 -0800

    finished main

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,17 +4,17 @@ class Main extends Component {
     render() {
         return (
             <section className="main-start-section hero is-fullheight is-fullheight-with-navbar">
-                {/* <div className="introduction-me"> */}
-
                 <img className="main-photo" src='IMG_6405.jpg' alt="Michael Chen" />
                 <div className="main-text-container">
                     <div className="main-text-section">
-                        <h1 className="main-header">Michael Chen</h1>
-                        <p className="main-paragraph">Hi! I'm Michael Chen and I'm a recent software engineer graduate from General Assembly</p>
+                        <div className="main-name">
+                            <h1 className="main-header">Michael</h1>
+                            <h1 className="main-header-last">Chen</h1>
+                        </div>
+                        <p className="main-paragraph">Hi! I'm Michael Chen and I'm a recent software engineer graduate from General Assembly.</p>
+                        <button className="button continue-more">Continue for More!</button>
                     </div>
                 </div>
-
-                {/* </div> */}
             </section>
         )
     }
