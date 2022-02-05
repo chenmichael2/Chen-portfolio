9e5bc6ae1b77a0b26d401bdc0dfa34330959f97f
chenmichael2 <chenmichael2012@gmail.com>
2022-02-05 12:33:21 -0800

    fixing the main portion, almost done

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,11 @@ class Main extends Component {
         return (
             <section className="main-start-section hero is-fullheight is-fullheight-with-navbar">
                 {/* <div className="introduction-me"> */}
+                <img className="main-photo" src='IMG_6405.jpg' alt="Michael Chen" />
+                <div className="main-text-section">
                     <h1 className="main-header">Michael Chen</h1>
-                    <img className="main-photo" src='grad-photo.png' alt="Michael Chen" />
+                    <p className="main-paragraph">Hi! I'm Michael Chen and I'm a recent software engineer graduate from General Assembly</p>
+                </div>
                 {/* </div> */}
             </section>
         )
