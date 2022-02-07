ed889f9217aa823101cd0183e0afd531b0f39c1e
chenmichael2 <chenmichael2012@gmail.com>
2022-02-07 12:23:15 -0800

    commiting start of about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,14 @@ import React, { Component } from 'react';
 
 class About extends Component {
     render() {
-        return(
+        return (
             <section className="about-start-section is-fullheight hero" id="about">
-                <h1 className="about-header">About</h1>
-                <h2 className="about-title">Who Am I?</h2>
-                <div className="about-paragraph">This is the paragraph where I talk more about myself</div>
-                <img src="About-photo.png" alt='About me photo' />
+                <div className="about-text-container">
+                    <h1 className="about-header">About</h1>
+                    <h2 className="about-title">Who Am I?</h2>
+                    <div className="about-paragraph">This is the paragraph where I talk more about myself</div>
+                </div>
+                <img className="about-photo" src="About-photo.png" alt='About me photo' />
 
             </section>
         )
