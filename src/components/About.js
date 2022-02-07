921f28223b921941b03e277377e87a1c4e4b55e8
chenmichael2 <chenmichael2012@gmail.com>
2022-02-07 12:42:07 -0800

    commiting background image opacity

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,12 @@ class About extends Component {
     render() {
         return (
             <section className="about-start-section is-fullheight hero" id="about">
+                <img className="about-photo" src="About-photo.png" alt='About me photo' />
                 <div className="about-text-container">
                     <h1 className="about-header">About</h1>
                     <h2 className="about-title">Who Am I?</h2>
                     <div className="about-paragraph">This is the paragraph where I talk more about myself</div>
                 </div>
-                <img className="about-photo" src="About-photo.png" alt='About me photo' />
-
             </section>
         )
     }
