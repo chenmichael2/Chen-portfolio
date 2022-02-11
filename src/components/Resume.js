7bfcb61a9725b5fd5d806ee7afcc628aea202b28
chenmichael2 <chenmichael2012@gmail.com>
2022-02-11 02:01:39 -0800

    commiting the way I kinda want the resume to look

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,12 +4,15 @@ class Resume extends Component {
     render() {
         return (
             <section className="resume-start-section is-fullheight hero" id="resume">
-                <div className="top-resume-section">
-                    <h1 className="resume-name">Michael Chen</h1>
-                    <h2 className="resume-bio">Software Engineer | Full-Stack Developer | Marine Reservist</h2>
-                </div>
                 <div className="resume-container">
-                    <div className="left-resume-section">left</div>
+                    <div className="resume-left">
+                        <div className="top-box-styling"></div>
+                        <div className="top-resume-section">
+                            <h1 className="resume-name">Michael Chen</h1>
+                            <h2 className="resume-bio">Software Engineer | Full-Stack Developer | Marine Reservist</h2>
+                        </div>
+                        <div className="left-resume-section">left</div>
+                    </div>
                     <div className="right-resume-section">right</div>
                 </div>
                 <a className="resume-download" href="Michael Chen Resume.pdf" target="_blank">Hello</a>
