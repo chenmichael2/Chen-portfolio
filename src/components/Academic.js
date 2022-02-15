3a3f8a1cc6f08fc9eb5d49160c4f8dbb848a0665
chenmichael2 <chenmichael2012@gmail.com>
2022-02-14 22:24:41 -0800

    adding Academics

---
+++ src/components/Academic.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Academic extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Academic;
