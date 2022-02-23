2d462295e63f408652188521dd6aec22c97137d5
chenmichael2 <chenmichael2012@gmail.com>
2022-02-23 15:37:30 -0800

    commiting mail to

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ class Contact extends Component {
                     <a href="https://github.com/chenmichael2"><i className="bi-github contact-icon"></i></a>
                     <a href="https://www.linkedin.com/in/chenmichael2/"><i className="bi-linkedin contact-icon"></i></a>
                 </div>
-                    <div className="contact-me-text">Contact Me!</div>
+                    <a href="mailto:chenmichael2012@gmail.com" className="contact-me-text">Contact Me!</a>
                 <div className="contact-me">
                     <div className="contact-container">
                         <div className='email'>Email: chenmichael2012@gmail.com</div>
